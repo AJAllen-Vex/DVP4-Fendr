8d21e7ead5c27f1774f5cfdd7fb259cc438930b8
AJ Allen <ajallen.vex@gmail.com>
2017-10-24 21:42:55 -0400

    Made a comment to test the group repository.

--- Fendr/Fendr/ViewController.swift
+++ Fendr/Fendr/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Testing changes made to the project - AJ
     }
 
     override func didReceiveMemoryWarning() {
